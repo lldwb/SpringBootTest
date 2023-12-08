8112bbcddace897b561fdb82b6284f4dcb64704c
安然的尾巴 <3247187440@qq.com>
2023-12-08 11:41:17 +0800

    修改为多模块

---
+++ src (1)/main/java/com/example/sptingboottest/SptingBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sptingboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SptingBootTestApplication.class, args);
+    }
+
+}
---
+++ src (1)/test/java/com/example/sptingboottest/SptingBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sptingboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptingBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
